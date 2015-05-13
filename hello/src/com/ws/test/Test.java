00e3c75414ded19315f59cae31811cd627fcfdff
booleaner <vip.boolean@gmail.com>
2015-05-13 23:45:57 +0800

    modify

--- hello/src/com/ws/test/Test.java
+++ hello/src/com/ws/test/Test.java
@@ -5,5 +5,26 @@ public class Test
 	public static void main(String[] args)
 	{
 		System.out.println ("test github...");
+		
+		if(isValid("name"))
+		{
+			
+		}
 	}
+
+	private static boolean isValid(String name)
+	{
+		System.out.println ( "Test.isValid()" );
+		boolean flag;
+		if("admin".equals ( name ))
+		{
+			flag=true;
+		}
+		else
+		{
+			flag=false;
+		}
+		return flag;
+	}
+	
 }
