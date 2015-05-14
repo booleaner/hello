e17d328d3f692ab981c1acd4cc8b449a26f8c61e
booleaner <vip.boolean@gmail.com>
2015-05-14 13:58:28 +0800

    changed

--- hello/src/com/ws/test/Second.java
+++ hello/src/com/ws/test/Second.java
@@ -2,5 +2,20 @@ package com.ws.test;
 
 public class Second
 {
+	public static void main(String[] args)
+	{
+		if(isValid("name"))
+		{
+			
+		}
+		else
+		{
+			
+		}
+	}
 
+	private static boolean isValid(String name)
+	{
+		return false;
+	}
 }
