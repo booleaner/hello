1c04f93c9064f6f54e1df2364ed4994e5d28067b
booleaner <vip.boolean@gmail.com>
2015-05-20 18:10:17 +0800

    modify

--- hello/src/com/ws/test/Student.java
+++ hello/src/com/ws/test/Student.java
@@ -8,5 +8,44 @@ public class Student
 	private int age;
 	private int sex;
 	
-	
+	public String getName()
+	{
+		return name;
+	}
+	public void setName(String name)
+	{
+		this.name = name;
+	}
+	public String getPhone()
+	{
+		return phone;
+	}
+	public void setPhone(String phone)
+	{
+		this.phone = phone;
+	}
+	public String getAddr()
+	{
+		return addr;
+	}
+	public void setAddr(String addr)
+	{
+		this.addr = addr;
+	}
+	public int getAge()
+	{
+		return age;
+	}
+	public void setAge(int age)
+	{
+		this.age = age;
+	}
+	public int getSex()
+	{
+		return sex;
+	}
+	public void setSex(int sex)
+	{
+		this.sex = sex;
+	}
 }
